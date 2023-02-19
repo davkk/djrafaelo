42a52f5d05e68dc2835e2fb7082c810dc86c1527
Dawid Karp <dadulerk@gmail.com>
2023-02-19 20:14:21 +0100

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,fs,fs.js}'],
+    theme: {
+        extend: {
+            colors: {
+                accent: "#CA9F54",
+                dark: "#0E0D0B",
+                light: "#FEF4E3",
+                sub: "#B0A38D",
+            },
+            fontFamily: {
+                sans: "Saira, sans-serif",
+                serif: "DM Serif Text, serif",
+            },
+            boxShadow: {
+                "glow": "0px 7px 30px -5px rgb(202, 159, 84, 0.6)",
+            },
+        },
+    },
+    plugins: [],
+}
